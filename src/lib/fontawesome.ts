555bb852fc1afe38c59c7ec27192c1268298e36e
Saiful Azhar <saifulazr@gmail.com>
2025-05-08 11:03:42 +0800

    Initial commit

---
+++ src/lib/fontawesome.ts
@@ -0,0 +1,41 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus,
+  faTrash,
+  faCheck,
+  faTimes,
+  faEdit,
+  faUser,
+  faSignOutAlt,
+  faSignInAlt,
+  faSpinner,
+  faExclamationCircle,
+  faCheckCircle,
+  faInfoCircle,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faTwitter,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'
+
+// Add icons to the library
+library.add(
+  // Solid icons
+  faPlus,
+  faTrash,
+  faCheck,
+  faTimes,
+  faEdit,
+  faUser,
+  faSignOutAlt,
+  faSignInAlt,
+  faSpinner,
+  faExclamationCircle,
+  faCheckCircle,
+  faInfoCircle,
+  // Brand icons
+  faGithub,
+  faTwitter,
+  faLinkedin
+) 
