829bb7f4aa64fbb808e00c1cea9ab0e197befcf0
Saiful Azhar <saifulazr@gmail.com>
2025-05-09 17:48:02 +0800

    add allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['3ggh9m-5173.csb.app']
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
