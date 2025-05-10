fce4c3fd5429fca3370b75bb54a92918ff58b975
Saiful Azhar <saifulazr@gmail.com>
2025-05-10 11:35:35 +0800

    Issue with allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr'
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
-    allowedHosts: ['*.csb.app', '*.codesandbox.io']
+    allowedHosts: ['frontend_web']
   },
   resolve: {
     alias: {
